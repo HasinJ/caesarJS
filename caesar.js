759916f71ac79211d9036c4b8a58886f66fe732e
HasinJ <hasinchoudhuryj@gmail.com>
2020-08-29 16:28:45 -0400

    first test

--- caesar.js
+++ caesar.js
@@ -1,5 +1,26 @@
-const caesar = function() {
+const caesar = function(string, shift) {
+  let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  let alphabetCapped = alphabet.join('').toUpperCase()
+  alphabetCapped=Array.from(alphabetCapped)
+  string = Array.from(string)
+
+  string = string.map(letter => {
+    let index = alphabet.indexOf(letter)
+
+    if (index==-1) {
+      index=alphabetCapped.indexOf(letter)
+      if (index==-1) return letter //means it's not a letter (punctuation or space)
+      return alphabetCapped[index+=shift]
+    }else {  return alphabet[index+shift]}
+  })
+
+  return string.join('')
+
 
 }
+
+
+
+//26 letters in alphabet
 
 module.exports = caesar
